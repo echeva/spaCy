f38e0c9c49c00da397e1db246e5958654ef319ab
echeva <yaniiecheva@gmail.com>
2022-03-23 11:37:48 -0300

    New document

---
+++ test/doc1.py
@@ -0,0 +1,17 @@
+text = "F. último doc.: 27/04/2020 08:08   SERVICIO DE ANATOMÍA PATOLÓGICA COORDINACIO ONCOLOGICA ONC ONCOLOGIA MEDIC " \
+       "B20-321815 Estudio: Centro: DEFINITIVO" \
+       "Se procede con los estudios moleculares Idylla EGFR, nCounter de pulmón y tinción por " \
+       "inmunohistoquímica de PD-L1 con el clon 22C3, de cuyo resultado se emitirá un informe complementario." \
+       "DESCRIPCIÓN MACROSCOPICA" \
+       "DETERMINACIÓ MOLECULAR" \
+       "Motivo: Determinación Molecular Correspondientes a: ONCOMINE, NANOSTRING. La muestra contiene un 70% de celularidad tumoral (tamaño 70mm2)." \
+       "- Diagnóstico histológico: " \
+       "neo de pulmón- Análisis solicitado:" \
+       "oncomine 22- Muestra recibida y analizada:" \
+       "B20-321815 que corresponde a bloque de archivo  B20-321815-A5- Porcentaje aproximado de células tumorales en muestras analizada:" \
+       "70%- Área aproximada de tejido analizada: 70mm2- Método: Extracción de ADN genómico del tumor." \
+       "Análisis mediante la ultrasecuenciación de un panel que analiza 22 genes. El panel analizado es el Oncomine Solid Tumour DNA de Applied que analiza mutaciones en los siguientes genes:" \
+       "AKT1, ALK, BRAF, CTNNB1, DDR2, EGFR, ERBB2, ERBB4, FBXW7, FGFR1, FGFR2, FGFR3, KRAS, MAP2K1, MET, NOTCH1, NRAS, PIK3CA, PTEN, SMAD4, STK11 y TP53" \
+       "- RESULTADO" \
+       "1. Genes MutadosCTNNB1 mutación exón 3; c.110C>T; p.(Ser37Phe)/p.(S37F); fecuencia aproximada alelo mutado 31% TP53 mutación exón 6; c.580C>T; p.(Leu194Phe)/p.(L194F); frecuencia aproximada alelo mutado 20%\n" \
+       "2. Resto de genes: No Mutados "
